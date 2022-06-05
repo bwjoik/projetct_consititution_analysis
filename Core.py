daeadd2bc3f7dfc8ceceb7c3c769636ec128be11
bwjoik <63500158+bwjoik@users.noreply.github.com>
2022-06-05 21:26:50 +0800

    Add files via upload

--- Core.py
+++ Core.py
@@ -29,95 +29,73 @@ def chinese_word_cut(mytext):
 
 #斷詞
 Raw_data["content_cutted"] = Raw_data.R.apply(chinese_word_cut)
-print(Raw_data["content_cutted"])
-print(Raw_data.shape)
-
-# def remove_stop_words(file_name,seg_list):
-#   with open(stopwords_path,'r') as f:
-#     stop_words = f.readlines()
-#   stop_words = [stop_word.rstrip() for stop_word in stop_words]
-#   for seg in seg_list:
-#     if seg not in stop_words:
-#       new_list.append(seg)
-# Raw_data["Filted"] = Raw_data.content_cutted.apply(remove_stop_words)
+cleared = pd.Series(index = range(0,819))
 # print(Raw_data["content_cutted"])
+# print(Raw_data.shape)
 
 
 # #停用詞讀取
 with open(stopwords_path,'r',encoding='utf-8-sig') as f:
     stop_words = f.readlines()
-    stop_words = [stop_word.rstrip() for stop_word in stop_words]
-new_list = []
+    stop_words = [stop_word.rstrip() for stop_word in stop_words] #讀取後不知道為什麼詞後面回有一個空格，移除掉
+
+
+
 # print(stop_words)
 # print(type(stop_words))
 
-# 停用詞處理
+# 停用詞過濾
+
 def stopword_filter(words):
+    new_list = []
     for seg in (words):
-        if seg not in stop_words:
-            new_list.append(seg)
+        for seg in seg:
+            if seg not in stop_words:
+                new_list.append(seg)
     return new_list
 
-Raw_data["Filted"] = Raw_data.content_cutted.apply(stopword_filter)
-print(Raw_data["Filted"])
+Filted = []
+for j in range(0,818):
+    Filted.append(stopword_filter(Raw_data.loc[j:j,"content_cutted"]))
+    
+Target_Data = pd.DataFrame({"Filted":Filted})
+# Processed_Data = pd.concat([Raw_data,Filted3],axis=1,sort=False)
 
 
+#向量化
+n_features = 10006
+tf_vectorizer = CountVectorizer(strip_accents = 'unicode',
+max_features=n_features,
+stop_words='english',
+max_df = 1,
+min_df = 1)
+tf = tf_vectorizer.fit_transform(Target_Data)
 
-# stopwords = []
-# with open(stopwords_path, 'r', encoding='utf-8') as f:
-#     for line in f:
-#         if len(line)>0:
-#             stopwords.append(line.strip())
+#主題抽取
+lda = LatentDirichletAllocation(n_components=20, max_iter=50,
+learning_method='online',
+learning_offset=50.,
+random_state=0)
 
+lda.fit(tf)
 
-  
+#主題抽取結果展示方法
+def print_top_words(model, feature_names, n_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("主題 #%d:" % topic_idx)
+        print(" ".join([feature_names[i]
+    for i in topic.argsort()[:-n_top_words - 1:-1]]))
+print()
 
-# Raw_data["Filted"] = Raw_data.content_cutted.apply(stopword_filter)
-# print(Raw_data["content_cutted"])
+#顯示個主題前X個詞
+n_top_words = 20
 
-# # print(stopwords)
-# def tokenizer(s):
-#     words = []
-#     if Raw_data not in stopwords:
-#         words.append(Raw_data)
-#     return words
+tf_feature_names = tf_vectorizer.get_feature_names()
+print_top_words(lda, tf_feature_names, n_top_words)
 
-# #向量化
-# n_features = 1000
-# tf_vectorizer = CountVectorizer(strip_accents = 'unicode',
-# max_features=n_features,
-# stop_words='english',
-# max_df = 0.5,
-# min_df = 10)
-# tf = tf_vectorizer.fit_transform(Raw_data.content_cutted)
+#主題抽取視覺化
+pyLDAvis.sklearn.prepare(lda, tf, tf_vectorizer)
 
-# #主題抽取
-# lda = LatentDirichletAllocation(n_components=20, max_iter=50,
-# learning_method='online',
-# learning_offset=50.,
-# random_state=0)
-
-# lda.fit(tf)
-
-# #主題抽取結果展示方法
-# def print_top_words(model, feature_names, n_top_words):
-#     for topic_idx, topic in enumerate(model.components_):
-#         print("主題 #%d:" % topic_idx)
-#         print(" ".join([feature_names[i]
-#     for i in topic.argsort()[:-n_top_words - 1:-1]]))
-# print()
-
-# #主題抽取展示
-
-# #顯示個主題前X個詞
-# n_top_words = 20
-
-# tf_feature_names = tf_vectorizer.get_feature_names()
-# print_top_words(lda, tf_feature_names, n_top_words)
-
-# #主題抽取視覺化
-# pyLDAvis.sklearn.prepare(lda, tf, tf_vectorizer)
-
-# #將圖像輸出到另一視窗(方便在不同環境下觀看)
-# data = pyLDAvis.sklearn.prepare(lda, tf, tf_vectorizer)
-# pyLDAvis.show(data)
+#將圖像輸出到另一視窗(方便在不同環境下觀看)
+data = pyLDAvis.sklearn.prepare(lda, tf, tf_vectorizer)
+pyLDAvis.show(data)
