632b32183629cf5026befa37305ded642b150bec
bwjoik <63500158+bwjoik@users.noreply.github.com>
2022-06-05 22:51:04 +0800

    Add files via upload

--- Core.py
+++ Core.py
@@ -11,13 +11,12 @@ from sklearn.decomposition import LatentDirichletAllocation
 import pyLDAvis
 import pyLDAvis.sklearn
 import csv
-# pyLDAvis.enable_notebook()
 
 
 # print(Raw_data.head())
 
 #詞庫引入
-dicts = "C:/Users/user/OneDrive - 東吳大學/python/projetct_consititution_analysis/text/中文分字字庫.txt"
+dicts = "C:/Users/user/OneDrive - 東吳大學/python/projetct_consititution_analysis/text/法律中文分字字庫.txt"
 stopwords_path = "C:/Users/user/OneDrive - 東吳大學/python/projetct_consititution_analysis/text/stopword.txt"
 Raw_data = pd.read_csv("C:/Users/user/OneDrive - 東吳大學/python/projetct_consititution_analysis/JR_text.csv",encoding='utf-8-sig')
 print(Raw_data.shape)
@@ -38,11 +37,11 @@ cleared = pd.Series(index = range(0,819))
 with open(stopwords_path,'r',encoding='utf-8-sig') as f:
     stop_words = f.readlines()
     stop_words = [stop_word.rstrip() for stop_word in stop_words] #讀取後不知道為什麼詞後面回有一個空格，移除掉
+stop_words.append('\n')
+stop_words.append('\n\n')
 
 
 
-# print(stop_words)
-# print(type(stop_words))
 
 # 停用詞過濾
 
@@ -63,16 +62,19 @@ Target_Data = pd.DataFrame({"Filted":Filted})
 
 
 #向量化
-n_features = 10006
+n_features = 10
 tf_vectorizer = CountVectorizer(strip_accents = 'unicode',
 max_features=n_features,
 stop_words='english',
 max_df = 1,
-min_df = 1)
+min_df = 1
+)
 tf = tf_vectorizer.fit_transform(Target_Data)
 
 #主題抽取
-lda = LatentDirichletAllocation(n_components=20, max_iter=50,
+lda = LatentDirichletAllocation(
+n_components=20,
+max_iter=50, 
 learning_method='online',
 learning_offset=50.,
 random_state=0)
